822d2adc652e4944d2757e6f0e1dff8d0e2623c1
foile <foile@mail.ru>
2019-10-16 08:52:52 +0300

    module1-task2

---
+++ src/components/app/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {number} from 'prop-types';
+import WelcomeScreen from '../welcome-screen.jsx';
+
+const App = (props) => {
+  const {gameTime, errorCount} = props;
+
+  return <WelcomeScreen
+    time={gameTime}
+    errorCount={errorCount}
+  />;
+};
+
+App.propTypes = {errorCount: number, gameTime: number};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './components/app/app.jsx';
+
+const init = () => {
+  const settings = {
+    gameTime: 5,
+    errorCount: 3,
+  };
+
+  ReactDOM.render(
+      <App
+        errorCount={settings.errorCount}
+        gameTime={settings.gameTime}
+      />,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
