2b677058d84e5e2a98adfd2b4ca770ee3b2a8019
foile <foile@mail.ru>
2019-10-20 02:23:26 +0300

    task2-1

--- src/components/screens/welcome-screen.e2e.test.js
+++ src/components/screens/welcome-screen.e2e.test.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import {WelcomeScreen} from './welcome-screen';
+import WelcomeScreen from './welcome-screen';
 Enzyme.configure({adapter: new Adapter()});
 
 it(`startButton correct run afrer click`, () => {
-  const clickHandler = jest.fn();
-  const welcomeScreen = shallow(<WelcomeScreen
-    mistakes={0}
-    minutes={0}
-    onClick={clickHandler}
-  />);
-  const startButton = welcomeScreen.find(`.welcome__button`);
+  const onClick = jest.fn();
+  const screen = shallow(
+      <WelcomeScreen
+        mistakes={0}
+        minutes={0}
+        onClick={onClick}
+      />);
+  const startButton = screen.find(`.welcome__button`);
   startButton.simulate(`click`);
-  expect(clickHandler).toHaveBeenCalledTimes(1);
+  expect(onClick).toHaveBeenCalledTimes(1);
 });
