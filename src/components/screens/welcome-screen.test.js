123cb1bdf9da66b8b1c2e93987f8f66d3f59f31a
foile <foile@mail.ru>
2019-10-21 00:42:49 +0300

    test

---
+++ src/components/screens/welcome-screen.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import renderer from "react-test-renderer";
+import WelcomeScreen from './welcome-screen';
+
+it(`App correctly renders after relaunch`, () => {
+  const tree = renderer
+  .create(<WelcomeScreen
+    errorCount={0}
+    time={0}
+    onClick={jest.fn()}
+  />)
+  .toJSON();
+  expect(tree).toMatchSnapshot();
+});
